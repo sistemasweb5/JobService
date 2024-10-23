d756b0252f1565e86a18d907c13778fd3299ef57
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-23 18:53:17 -0400

    build: added geolocalization to job table

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
 CREATE TABLE IF NOT EXISTS jobs (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_client_id UUID NOT NULL,
@@ -6,5 +8,6 @@ CREATE TABLE IF NOT EXISTS jobs (
     job_type VARCHAR(50) NOT NULL, -- 'plumber', 'electrician', etc.
     status VARCHAR(20) NOT NULL CHECK (status IN ('not assigned', 'assigned', 'on the way', 'working', 'done')),
     description TEXT,
-    price DECIMAL(10, 2) -- 10 digits in total and 2 decimals
+    price DECIMAL(10, 2), -- 10 digits in total and 2 decimals
+    location geography(Point) -- Coordinates in WGS 84 (latitude/longitude)
 );
