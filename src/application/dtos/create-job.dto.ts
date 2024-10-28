1f185b3789e08184e4962622f26ff813ac19cbff
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-10-28 19:34:11 -0400

    feat: validations on dto implemented

--- src/application/dtos/create-job.dto.ts
+++ src/application/dtos/create-job.dto.ts
@@ -1,10 +1,48 @@
+import { IsDecimal, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, Max, MaxLength, Min } from "class-validator";
+
+export enum JobStatus {
+  NOT_ASSIGNED = 'not assigned',
+  ASSIGNED = 'assigned',
+  ON_THE_WAY = 'on the way',
+  WORKING = 'working',
+  DONE = 'done'
+}
+
 export class CreateJobDto {
+  @IsUUID()
+  @IsNotEmpty()
   userClientId: string;
+
+  @IsUUID()
+  @IsOptional()
   userWorkerId?: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   jobType: string;
+
+  @IsEnum(JobStatus)
+  @IsNotEmpty()
+  @MaxLength(20)
   status: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNumber()
+  @Min(0)
+  @IsDecimal({ decimal_digits: '2' })
   price: number;
+
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
   latitude: number;
+  
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
   longitude: number;
 }
--- src/presentation/graphql/inputs/create-job.input.ts
+++ src/presentation/graphql/inputs/create-job.input.ts
@@ -1,28 +1,51 @@
 import { InputType, Field, Float } from "@nestjs/graphql";
+import { IsDecimal, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, MaxLength, Min } from "class-validator";
+import { JobStatus } from "src/application/dtos/create-job.dto";
 
 @InputType()
 export class CreateJobInput {
   @Field()
+  @IsUUID()
+  @IsNotEmpty()
   userClientId: string;
 
   @Field({ nullable: true })
+  @IsUUID()
+  @IsOptional()
   userWorkerId?: string;
 
   @Field()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   jobType: string;
 
   @Field()
+  @IsEnum(JobStatus)
+  @IsNotEmpty()
+  @MaxLength(20)
   status: string;
 
   @Field()
+  @IsString()
+  @IsNotEmpty()
   description: string;
 
   @Field(() => Float)
+  @IsNumber()
+  @Min(0)
+  @IsDecimal({ decimal_digits: '2' })
   price: number;
 
   @Field(() => Float)
+  @IsNumber()
+  @Min(-90)
+  @Min(90)
   latitude: number;
 
   @Field(() => Float)
+  @IsNumber()
+  @Min(-180)
+  @Min(180)
   longitude: number;
 }
