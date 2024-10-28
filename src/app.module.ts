e4ed0bed98cc71a7a434b1b4f19f9114367728de
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-10-28 13:06:45 -0400

    feat: modules implemented to inyection dependency

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { JobModule } from './job.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+      introspection: true,
+    }),
+    JobModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
