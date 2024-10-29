632b8c23d66e23c56b561cbb8f04e585b12fead2
JosuePrado <prado69508698@gmail.com>
2024-10-28 20:02:42 -0400

    feat: implement update a job

---
+++ src/application/use-cases/update-job.use-case.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IJobRepository } from 'src/domain/repositories/job.repository.interface';
+import { CreateJobDto } from '../dtos/create-job.dto';
+import { Job } from 'src/domain/entities/job.entity';
+import { Location } from 'src/domain/value-objects/location.value-object';
+
+@Injectable()
+export class UpdateJobUseCase {
+  constructor(
+    @Inject('IJobRepository')
+    private readonly jobRepository: IJobRepository,
+  ) {}
+
+  async execute(id: string, dto: CreateJobDto): Promise<Job> {
+    const job = new Job({
+      userClientId: dto.userClientId,
+      userWorkerId: dto.userWorkerId,
+      jobType: dto.jobType,
+      status: dto.status,
+      description: dto.description,
+      price: dto.price,
+      createdAt: new Date(),
+      location: new Location(dto.latitude, dto.longitude),
+    });
+
+    return this.jobRepository.update(id, job);
+  }
+}
--- src/domain/repositories/job.repository.interface.ts
+++ src/domain/repositories/job.repository.interface.ts
@@ -2,5 +2,6 @@ import { Job } from "../entities/job.entity";
 
 export interface IJobRepository {
   create(job: Job): Promise<Job>;
+  update(idActualJob: string, job: Job): Promise<Job>;
   findById(id: string): Promise<Job | null>;
 }
